7dbc805e4dd0dfa74be0e2c45ba19ed3516ba520
emkay27 <ace27mk@gmail.com>
2019-04-03 17:02:11 +0200

    Added a comment to the down_vote.php file

--- voting/down_vote.php
+++ voting/down_vote.php
@@ -1,15 +1,16 @@
 <?php
 include("config.php");
-$ip=$_SERVER['REMOTE_ADDR']; 
+$ip=$_SERVER['REMOTE_ADDR'];
 if($_POST['id'])
 {
 $id=$_POST['id'];
 $id = mysql_escape_String($id);
 $ip_sql=mysql_query("select ip_add from Voting_IP where mes_id_fk='$id' and ip_add='$ip'");
 $count=mysql_num_rows($ip_sql);
+
 if($count==0)
 {
-$sql = "update Messages set down=down+1  where mes_id='$id'";
+$sql = "update Messages set down=down+1  where mes_id='$id'";//This will add a 1 to the down table;
 mysql_query( $sql);
 
 ////////     Removing this provides a user to Dislike a Single Post Multiple Times    //////////
