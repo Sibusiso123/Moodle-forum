b143b17b4e24d6036386df5859eca0effc30972e
khomotjo4 <k.mathabatha4@gmail.com>
2019-04-06 21:11:15 +0200

    Added a comments to the database.sql file

--- new_voting/database.sql
+++ new_voting/database.sql
@@ -1,17 +1,17 @@
-CREATE TABLE `posts` (
+CREATE TABLE `posts` (/*posts is the table*/
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `text` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE `rating_info` (
+CREATE TABLE `rating_info` (/*rating_info is the table*/
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   `rating_action` varchar(30) NOT NULL,
    CONSTRAINT UC_rating_info UNIQUE (user_id, post_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `posts` (`id`, `text`) VALUES
+INSERT INTO `posts` (`id`, `text`) VALUES 
 (1, 'Moodle Upvote'),
 (2, 'Moodle Downvote'),
 (3, 'Forum'),
