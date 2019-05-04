48e0e89b501998eabe498624fed234f9ffbc5bb7
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-04 22:57:49 +0200

    Update test_server.php

--- new_voting/unitTest/test_server.php
+++ new_voting/unitTest/test_server.php
@@ -2,7 +2,7 @@
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 #require_once('locallib.php');
-class locallibTest extends TestCase{
+class serverTest extends TestCase{
   
  
  
