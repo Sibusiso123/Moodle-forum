d35cf5a6d56681a46dd27361c42a5b36079d0269
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-05 01:06:59 +0200

    Update test_index.php

--- new_voting/unitTests/test_index.php
+++ new_voting/unitTests/test_index.php
@@ -1,5 +1,12 @@
 <?php
-class index extends \PHPUnit_Framework_TestCase{
-
-//
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+#require_once('locallib.php');
+class indexTest extends TestCase{
+  
+ public function test_tests(){
+   $temp=tests();
+ $this->assertEquals(1,1, "correct!"); 
+ }
+ 
 }
