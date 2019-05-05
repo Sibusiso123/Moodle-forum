7d0147da26f056533214007787bcff4c9402342e
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-05 22:19:00 +0200

    Update test_server.php

--- new_voting/unitTests/test_server.php
+++ new_voting/unitTests/test_server.php
@@ -6,7 +6,7 @@ class serverTest extends TestCase{
   
  public function test_test(){
   // $temp=test();
-   echo "This works just fine";
+   echo "This works just fine but coverage is 0.0%";
  $this->assertEquals(1,1, "correct!"); 
  }
  
