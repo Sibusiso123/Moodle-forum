3460f0142a904dad04ad55e45ba47bccf28542a3
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-05 22:47:45 +0200

    Update test_index3.php

--- new_voting/unitTests/test_index3.php
+++ new_voting/unitTests/test_index3.php
@@ -5,11 +5,10 @@ use PHPUnit\DbUnit\TestCaseTrait;
 class index3Test extends TestCase{
   
  public function test_number(){
- //  echo (getcwd());
    require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/index3.php');
-   number(2,3);
+   $test=number(2,3);
    echo "It works but doesn't show on the coverall";
- $this->assertEquals(1,1, "correct!"); 
+ $this->assertEquals(5,$test, "correct!"); 
  }
  
 }
