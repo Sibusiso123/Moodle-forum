6309200ef395a8c1ce82439c77cfaa168a28fb09
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-05 22:40:21 +0200

    Update test_index3.php

--- new_voting/unitTests/test_index3.php
+++ new_voting/unitTests/test_index3.php
@@ -6,7 +6,7 @@ class index3Test extends TestCase{
   
  public function test_number(){
    echo (dgetcwd());
-   require_once('Moodle-forum/new_voting/index3.php');
+   require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/index3.php');
    number();
    echo "It works but doesn't show on the coverall";
  $this->assertEquals(1,1, "correct!"); 
