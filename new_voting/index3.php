a8de87b8821a1ffe713f44aaf4336c07c99b16e0
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-05 22:46:52 +0200

    Update index3.php

--- new_voting/index3.php
+++ new_voting/index3.php
@@ -1,53 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Learning Php Part4</title>
-</head>
-<form method="GET">
-	<input type="number" name="num1">
-	<!--	<button>submit1</button> -->
-	<input type="number" name="num2">
-	<button>submit</button>
-
-</form>
-
-
-<body>
-
 <?php
-	//Comparison Operators
 
-	//sleep(10);
   function number($num1,$num2){
-	$Ans1=$num1+$num2;
-	$Ans2=$num1-$num2;
-	$Ans3=$num1/$num2;
-	$Ans4=$num1*$num2;
-	if($num1<$num2){
-
-		echo "Its true that num1 is less than num2"."<br/>";
-	}
-	if(num1>0){
-	        echo "num1 is a positive value"."<br/>";
-	}
-
-	if ($num1<0) {
-
-		echo "num1 is negative"."<br/>";
-	}
-	if($num1>$num2){
-		echo "num1 is greater than num2";
-	}
-	if($num1==$num2){
-		echo "num1 is equal to num2";
-	}
-	 else{
-	 	echo "<br/>"."No options";
-	 }
-
-	echo "<br/>".$Ans;
+	return $num1+$num2;
   }
 ?>
-
-</body>
-</html>
