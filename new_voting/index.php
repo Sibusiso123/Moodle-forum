a6bd58b23aa0300b21cc0c44f2e256dbd620fbb9
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-06 02:22:36 +0200

    Update index.php

--- new_voting/index.php
+++ new_voting/index.php
@@ -38,7 +38,15 @@
           echo "<br/>"."<br/>"."Some Students have voted";
               //echo "<br/>".max(array(getLikes($post['id'])+ getDislikes($post['id'])));
         }
-	function tests($getLikes($post['id']),$getDislikes($post['id'])){
-  		return $getLikes($post['id'])+ $getDislikes($post['id']);
+	function tests($postlike,$postdislike){
+  		if($postlike>$postdislike){
+			return $postlike;
+		}
+		else if($postlike<$postdislike){
+			return $postdislike;
+		}
+		else{
+			return $postlike+$postdislike;
+		}
 	}
        ?>
