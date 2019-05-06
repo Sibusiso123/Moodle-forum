c8ccae31fdab0d9b992171e9ab712dca431c60d5
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-06 02:28:05 +0200

    Update index.php

--- new_voting/index.php
+++ new_voting/index.php
@@ -1,43 +1,4 @@
-   <?php foreach ($posts as $post): ?>
-   	<div class="post">
-      <?php echo $post['text']; ?>
-      <div class="post-info">
-	    <!-- if user likes post, style button differently -->
-      	<i <?php if (userLiked($post['id'])): ?>
-      		  class="fa fa-arrow-circle-up like-btn"
-      	  <?php else: ?>
-      		  class="fa fa-arrow-circle-o-up like-btn"
-      	  <?php endif ?>
-      	  data-id="<?php echo $post['id'] ?>"></i>
-      	<span class="likes"><?php echo getLikes($post['id']); ?></span>
-
-      	&nbsp;&nbsp;&nbsp;&nbsp;
-
-	    <!-- if user dislikes post, style button differently -->
-      	<i
-      	  <?php if (userDisliked($post['id'])): ?>
-      		  class="fa fa-arrow-circle-down dislike-btn"
-      	  <?php else: ?>
-      		  class="fa fa-arrow-circle-o-down dislike-btn"
-      	  <?php endif ?>
-      	  data-id="<?php echo $post['id'] ?>"></i>
-      	<span class="dislikes"><?php echo getDislikes($post['id']); ?></span>
-      </div>
-      <span class="likes"><p></p>    <?php echo "Total Number of Votes so far : "; echo(getLikes($post['id'])+ getDislikes($post['id']));
-      ?></span>
-      <?php
-        if(getLikes($post['id'])+ getDislikes($post['id']) == 0){
-          echo "<br/>"."<br/>"."No Student has voted";
-              //echo "<br/>".max(array(getLikes($post['id'])+ getDislikes($post['id'])));
-        }
-        else if(getLikes($post['id'])+ getDislikes($post['id']) == 1){
-          echo "<br/>"."<br/>"."A Student has voted";
-              //echo "<br/>".max(array(getLikes($post['id'])+ getDislikes($post['id'])));
-        }
-        else if(getLikes($post['id'])+ getDislikes($post['id']) >1){
-          echo "<br/>"."<br/>"."Some Students have voted";
-              //echo "<br/>".max(array(getLikes($post['id'])+ getDislikes($post['id'])));
-        }
+   <?php
 	function tests($postlike,$postdislike){
   		if($postlike>$postdislike){
 			return $postlike;
