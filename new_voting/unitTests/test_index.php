6e0aacc062b772bc99307511c4ded6d975ac7f3a
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-24 21:43:24 +0200

    Update test_index.php

--- new_voting/unitTests/test_index.php
+++ new_voting/unitTests/test_index.php
@@ -6,8 +6,8 @@ class indexTest extends TestCase{
   
  public function test_tests(){
    require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/index.php');
-   $test=tests(8,3);
- $this->assertEquals(8,$test, "correct!"); 
+   $test=tests(3,3);
+ $this->assertEquals(3,$test, "correct!"); 
  }
  
 }
