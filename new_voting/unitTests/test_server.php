4ca3c942225a65d7ce018500470f2993f2845828
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-25 00:01:08 +0200

    Update test_server.php

--- new_voting/unitTests/test_server.php
+++ new_voting/unitTests/test_server.php
@@ -5,8 +5,6 @@ use PHPUnit\DbUnit\TestCaseTrait;
 class serverTest extends TestCase{
   
  public function test_test(){
-  // $temp=test();
-   echo "This works just fine but coverage is 0.0%";
  $this->assertEquals(1,1, "correct!"); 
  }
  
