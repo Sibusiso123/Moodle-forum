febdf9f23a32229853804925c50824ac64112c5c
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-25 00:02:21 +0200

    Update test_User.php

--- new_voting/unitTests/test_User.php
+++ new_voting/unitTests/test_User.php
@@ -4,7 +4,7 @@ use PHPUnit\DbUnit\TestCaseTrait;
 #require_once('locallib.php');
 class indexTest extends TestCase{
   
- public function test_tests(){
+ public function test_user(){
    require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/User.php');
    $test=user(12,8,3,24);
  $this->assertEquals(32,5,$test, "correct!"); 
