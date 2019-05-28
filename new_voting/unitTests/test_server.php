af732c246982f68fe7bd6ee9acdbb8391452c782
BawinileM <47977371+BawinileM@users.noreply.github.com>
2019-05-29 00:30:32 +0200

    Update test_server.php

--- new_voting/unitTests/test_server.php
+++ new_voting/unitTests/test_server.php
@@ -6,13 +6,18 @@ class serverTest extends TestCase{
   
  public function test_test(){
    require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/server.php');
-   $test=number(13,13);
- $this->assertEquals(13,$test, "correct!");
+   $test=number(13,10);
+ $this->assertEquals(23,$test, "correct!");
  //$this->assertEquals(13,13, "correct!"); 
  }
   public function test_getLikes(){
     require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/server.php');
-    $test=getlikes(1,1);
- $this->assertEquals(1,$test, "correct!"); 
+    $test=getlikes(1,2);
+ $this->assertEquals(3,$test, "correct!"); 
+ }
+    public function test_getDislikes(){
+    require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/server.php');
+    $test=getlikes(1,2);
+ $this->assertEquals(2,$test, "correct!"); 
  }
 }
