93ab397685bb89a65c2e993384e8fae08e66a245
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-29 00:59:56 +0200

    Update test_server.php

--- new_voting/unitTests/test_server.php
+++ new_voting/unitTests/test_server.php
@@ -6,18 +6,18 @@ class serverTest extends TestCase{
   
  public function test_test(){
    require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/server.php');
-   $test=number(13,10);
+   $test=test(13,10);
  $this->assertEquals(23,$test, "correct!");
  //$this->assertEquals(13,13, "correct!"); 
  }
   public function test_getLikes(){
     require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/server.php');
-    $test=getlikes(1,2);
+    $test=getLikes(1,2);
  $this->assertEquals(3,$test, "correct!"); 
  }
     public function test_getDislikes(){
     require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/server.php');
-    $test=getlikes(1,2);
+    $test=getDisLikes(1,2);
  $this->assertEquals(2,$test, "correct!"); 
  }
 }
