f80ee7a315f0e4611d1c4b7fa15659629e3c7ab1
Khomotjo4 <30465580+Khomotjo4@users.noreply.github.com>
2019-05-30 01:42:11 +0200

    Create test_locallib_test.php

---
+++ moodleforum/tests/UnitTests2/test_locallib_test.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+#require_once('locallib.php');
+class indexTest extends TestCase{
+  
+ public function test_test_moodleforum_disallow_subscribe_on_create(){
+   require_once('/home/travis/build/hex-hypercity/Moodle-forum/moodleforum/tests/locallib_test.php');
+   $test=test_moodleforum_disallow_subscribe_on_create(1,1);
+ $this->assertEquals(1,$test, "correct!"); 
+ }
+ 
+}
