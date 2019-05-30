c11a16045cb0d2f5cf3c3a514e9a4fee18be4c9c
Khomotjo4 <30465580+Khomotjo4@users.noreply.github.com>
2019-05-30 23:54:47 +0200

    Update index3.php

--- new_voting/index3.php
+++ new_voting/index3.php
@@ -1,20 +1,27 @@
 <?php
-
-  function number($num1,$num2){
-	  if($num1>$num2){
-		  $sum=0;
-		return $num1+$num2;
-	  }
-	
-	  else if($num1<$num2){
-		  return $num2;
-	  }
-	  else if($num1==$num2){
-		  return $num1;
-	  }
-	  else if($num1 != 100){
-	  	return 0;
-	  }
-	
-  }
+include_once __DIR__ .'../../../post.php';
+class postTest extends PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers downvote
+     */
+    public function testUpvote()
+    {
+        $upvote = new Upvote();
+        $data =[2,3,4];
+        $this->assertNotEmpty($upvote->getAverage($data));
+        $this->assertEmpty($upvote->getAverage([]));
+    }
+    /**
+     * @covers upvote
+     */
+    public function testDownvote()
+    {
+        $downvote = new Downvote();
+        $val =1;
+        $this->assertNotEmpty($downvote->get_numeric($val));
+        //test fail
+        $this->assertEmpty($downvote->get_numeric("a"));
+    }
+}
 ?>
