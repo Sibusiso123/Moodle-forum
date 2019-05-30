f97f1649d2d95f2e117b8746547820d20dd41acb
Sibusiso123 <47978878+Sibusiso123@users.noreply.github.com>
2019-05-30 03:29:40 +0200

    Update test_index3.php

--- new_voting/unitTests/test_index3.php
+++ new_voting/unitTests/test_index3.php
@@ -6,8 +6,8 @@ class index3Test extends TestCase{
   
  public function test_number(){
    require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/index3.php');
-   $test=number(5,100);
- $this->assertEquals(100,$test, "correct!"); 
+   $test=number(2,3);
+ $this->assertEquals(3,$test, "correct!"); 
  }
  
 }
