3adda2a8971c4196d8cd29c108d746d01c0e19c2
Khomotjo4 <30465580+Khomotjo4@users.noreply.github.com>
2019-05-30 23:52:02 +0200

    Update test_index3.php

--- new_voting/unitTests/test_index3.php
+++ new_voting/unitTests/test_index3.php
@@ -1,11 +1,39 @@
 <?php
+    
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
-#require_once('locallib.php');
-class postTest extends TestCase{
+#require_once('config.php');
+class index3Test extends TestCase{
+    
   
- public function TestUpvote(){
-   require_once('/home/travis/build/hex-hypercity/Moodle-forum/tests/config.php');
- }
- 
-}
+ public function test_tests(){
+     
+   require_once('/home/travis/build/hex-hypercity/Moodle-forum/new_voting/index3.php');  
+    
+    private $host = 'localhost';
+    private $name = 'voting';
+    private $user = 'root';
+    private $user_='admin';
+    private $pass = '';
+    
+    /**
+     * @covers Database::connect
+     */
+    public function testConnect()
+    {
+        $database = new Database($this->host, $this->name, $this->user, $this->pass);
+        $this->assertNotEmpty($database->connect());
+    }
+    public function testFailConnect()
+    {
+        
+        $database = new Database($this->host,$this->name,$this->user_,$this->pass);
+        $conn=$database->connect();
+        if($conn instanceof \Exception){
+            
+            throw $conn;
+            }
+        $this->assertNull($conn); 
+        }
+    }
+}   
