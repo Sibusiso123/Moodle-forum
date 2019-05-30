19bc14ac3fa715257226a65342569df8853f5304
Khomotjo4 <30465580+Khomotjo4@users.noreply.github.com>
2019-05-30 02:58:48 +0200

    Update test_locallib.php

--- new_voting/unitTests/test_locallib.php
+++ new_voting/unitTests/test_locallib.php
@@ -4,9 +4,9 @@ use PHPUnit\DbUnit\TestCaseTrait;
 #require_once('locallib.php');
 class locallibTest extends TestCase{
   
- public function test_test_moodleforum_disallow_subscribe_on_create(){
+ public function test_test_moodleforum_auto_subscribe_on_create(){
    require_once('/home/travis/build/hex-hypercity/Moodle-forum/moodleforum/tests/locallib.php');
-   $test=test_moodleforum_disallow_subscribe_on_create(1,1);
+   $test=test_moodleforum_auto_subscribe_on_create(1,1);
  $this->assertEquals(1,$test, "correct!"); 
  }
  
