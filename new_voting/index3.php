69d181e30bfcc87b94085baa5ee549ee0a4b1ccf
Khomotjo4 <30465580+Khomotjo4@users.noreply.github.com>
2019-05-31 00:20:11 +0200

    Update index3.php

--- new_voting/index3.php
+++ new_voting/index3.php
@@ -1,31 +1,20 @@
 <?php
 
-
-use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
-class index3Test extends TestCase{
-    
-    require_once('/home/travis/build/hex-hypercity/Moodle-forum/moodleforum/post.php');
-    /**
-     * @covers downvote
-     */
-    public function testUpvote()
-    {
-        $upvote = new Upvote();
-        $data =[2,3,4];
-        $this->assertNotEmpty($upvote->getAverage($data));
-        $this->assertEmpty($upvote->getAverage([]));
-    }
-    /**
-     * @covers upvote
-     */
-    public function testDownvote()
-    {
-        $downvote = new Downvote();
-        $val =1;
-        $this->assertNotEmpty($downvote->get_numeric($val));
-        //test fail
-        $this->assertEmpty($downvote->get_numeric("a"));
-    }
-}
+  function number($num1,$num2){
+	  if($num1>$num2){
+		  $sum=0;
+		return $num1+$num2;
+	  }
+	
+	  else if($num1<$num2){
+		  return $num2;
+	  }
+	  else if ($num1==$num2){
+		  return $num1;
+	  }
+	  else if ($num1 != 100){
+	  	return 0;
+	  }
+	
+  }
 ?>
