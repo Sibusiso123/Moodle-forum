731b053a4a78e7fab9f7dfda790b6da7127f72c5
Emkay27 <47977373+Emkay27@users.noreply.github.com>
2019-05-31 03:42:50 +0200

    Update index3.php

--- new_voting/index3.php
+++ new_voting/index3.php
@@ -1,20 +1,16 @@
 <?php
 
   function number($num1,$num2){
-	  if($num1>$num2){
+	  if($num1>$num2 && $num1 < 4){
 		  $sum=0;
 		return $num1+$num2;
 	  }
 	
-	  else if($num1<$num2){
+	  else if($num1<$num2 && $num2 > 4){
 		  return $num2;
 	  }
-	  else if ($num1==$num2){
+	  else if ($num1==$num2 || $num == 0){
 		  return $num1;
 	  }
-	  else if ($num1 != 100){
-	  	return 0;
-	  }
-	
   }
 ?>
